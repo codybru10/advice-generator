bd0263da8e6621db38bbd8a087434b9d433a4393
Cody Brubaker and Jim Padilla <jim@>
2016-10-25 14:35:52 -0700

    Add jQuery to score user inputs and give advice

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
-$(document).ready(function() {
 
+var adviceGen = function(result) {
+  if (result > 4){
+    var advice = "Seek immediate help!";
+    return advice;
+  } else if (result <= 4) {
+    var advice = "You're normal";
+    return advice;
+  }
+};
+
+$(document).ready(function() {
+  $("form#stress_test").submit(function(event) {
+    event.preventDefault();
+    
+    $("#output").show();
+    $("#stress_test").hide();
+
+    var counterWarnings = 0;
+    $("input:checkbox[name=warning-signs]:checked").each(function() {
+      counterWarnings++;
+    });
+
+    var counterSymptoms = 0
+    $("input:checkbox[name=symptoms]:checked").each(function(){
+      counterSymptoms++;
+    });
+
+    var counterCope = 0
+    $("input:checkbox[name=cope]:checked").each(function(){
+      counterCope++;
+    });
+
+
+    var badResult = counterWarnings + counterSymptoms;
+    var totalResult = badResult - counterCope;
+
+    $("#adviceOutput").append("<p>" +"Your total score is " + totalResult + "</p>" + "<br>");
+    var advice = adviceGen(totalResult);
+    $("#adviceOutput").append(advice);
+
+  });
 });
